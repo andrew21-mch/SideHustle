3932ad38c299ace0b95766cc81140839b1c24897
nfon andrew <58951422+andrew21-mch@users.noreply.github.com>
2022-04-25 00:34:46 +0100

    Delete GIP directory

--- GIP/conect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$dbserver = "localhost";
-$dbUsername = "root";
-$dbpassword="";
-$dbName="hostpital";
-
-$conn = mysqli_connect($dbserver,$dbUsername,$dbpassword,$dbName);
-?>
--- GIP/register.php
+++
@@ -1,8 +0,0 @@
-<?php
-$serverName = "localhost";
-$userName = "root";
-$password="";
-$dbName ="hostpital";
-$conn = mysqli_connect($serverName, $userName, $password, $dbName);
-
-?>
